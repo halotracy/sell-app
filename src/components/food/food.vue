9713658716051656d255bc54b5f464565c44f1be
rambo <laiqing1991@outlook.com>
2017-04-16 14:00:53 +0800

    add the ratings toggle and time

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -33,12 +33,12 @@
         <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
         <div class="rating-wrapper">
           <ul v-show="food.ratings && food.ratings.length">
-            <li v-for="rating in food.ratings" class="rating-item border-1px">
+            <li v-show="needShow(rating.rateType,rating.text)" v-for="rating in food.ratings" class="rating-item border-1px">
               <div class="user">
                 <span class="name">{{rating.username}}</span>
                 <img class="avatar" width="12" height="12" :src="rating.avatar" >
               </div>
-              <div class="time">{{rating.rateTime}}</div>
+              <div class="time">{{rating.rateTime | formatDate}}</div>
               <p class="text">
                 <span :class="{'icon-thumb_up': rating.rateType === 0,'icon-thumb_down': rating.rateType === 1}"></span>{{rating.text}}
               </p>
@@ -46,7 +46,7 @@
 
           </ul> 
         </div>
-        <div class="no-rating" v-show="!food.rating || !food.rating.length"></div>
+        <div class="no-rating" v-show="!food.ratings || !food.ratings.length">暂无评价</div>
       </div>
     </div>
   </div>
@@ -56,6 +56,7 @@
 import BScroll from "better-scroll";
 import cartcontrol from "components/cartcontrol/cartcontrol"
 import Vue from "vue";
+import {formatDate} from "common/js/date";
 import split from "components/split/split";
 import ratingselect from "components/ratingselect/ratingselect";
 
@@ -86,7 +87,7 @@ const ALL = 2;
         this.showFlag = true;
         //console.log('showFlag',this.showFlag)
         this.selectType = ALL;
-        this.onlyContent = true;
+        this.onlyContent = false;
         this.$nextTick(() => {
             if(!this.scroll) {
               this.scroll = new BScroll(this.$els.food,{
@@ -107,6 +108,38 @@ const ALL = 2;
         this.$dispatch('cart.add',event.target);
         Vue.set(this.food,'count',1);
         console.log('addFirst',event.target);
+      },
+      needShow(type,text) {
+        if(this.onlyContent && !text){
+          return false;
+        }
+        if(this.selectType === ALL ){
+          return true;
+        } else {
+          return type === this.selectType;
+        }
+
+      },
+    },
+    events: {
+      'ratingtype.select'(type) {
+        this.selectType = type;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        })
+      },
+      'content.toggle'(onlyContent) {
+        this.onlyContent = onlyContent;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        })
+      }
+
+    },
+    filters: {
+      formatDate(time) {
+        let date = new Date(time);
+        return formatDate(date,'yyyy-MM-dd hh:mm');
       }
     },
     components: {
@@ -257,13 +290,16 @@ const ALL = 2;
           color: rgb(7,17,27);
           .icon-thumb_up, .icon-thumb_down
             margin-right: 4px;
-            line-height: 24px;
+            line-height: 16px;
             font-size: 12px;
           .icon-thumb_up
             color: rgb(0,160,220);
           .icon-thumb_down
             color: rgb(147,153,159);
-
+      .no-rating
+        padding: 16px 0;
+        font-size: 12px;
+        color: rgb(147,153,159);
 
     
 </style>
