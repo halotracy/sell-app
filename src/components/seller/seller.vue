242c9fbea209b6f6d47dce4df60f38de188f1ee1
rambo <laiqing1991@outlook.com>
2017-04-17 23:07:48 +0800

    add seller info

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -53,6 +53,13 @@
           </ul>
         </div>
       </div>
+      <split></split>
+      <div class="info">
+        <h1 class="tilte border-1px">商家信息</h1>
+        <ul>
+          <li class="info-item" v-for="info in seller.infos">{{info}}</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -236,5 +243,19 @@ export default {
           height: 90px;
           &:last-child
             margin: 0;
-
+  .info
+    padding: 18px 18px 0 18px;
+    .title
+      padding-bottom: 12px;
+      line-height: 14px;
+      color: rgb(7,17,27);
+      border-1px(rgba(7,17,27,0.1));   
+      font-size: 14px;
+    .info-item
+        padding: 16px 12px;
+        line-height: 16px;
+        border-1px(rgba(7,17,27,0.1));
+        font-size: 12px;
+        &:last-child
+          border-none();
 </style>
